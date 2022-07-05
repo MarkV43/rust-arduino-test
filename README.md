bcec2e7f34d854b54e0c92f249c866d6c467e66b
Marcelo Vogt <marcelo.vogt@explot.com>
2022-07-05 09:31:40 -0300

    Fixed compilation issues and testing markup math

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-`avr-hal-template`
+Rust Controller
 ==================
-[`cargo-generate`] template for jumpstarting projects on common AVR
-microcontroller boards.  This template supports the following hardware at this
-time:
-
- - Arduino Leonardo
- - Arduino Mega 2560
- - Arduino Nano
- - Arduino Uno
- - SparkFun ProMicro
+A project written in Rust for Arduino Uno with the purpose of being a feedback controller for any system. This project proposes a template implementation for discrete systems.
 
 ## Usage
 If you don't have them already, install [`cargo-generate`] and [`ravedude`]:
@@ -18,14 +10,7 @@ cargo install cargo-generate
 cargo install ravedude
 ```
 
-Then instanciate this template:
-
-```bash
-cargo generate --git https://github.com/Rahix/avr-hal-template.git
-```
-
-You will be prompted to select your board - do so and you're ready to roll!
-Everything is prepared so you should be able to just
+You should be able to just
 
 ```bash
 cargo run
@@ -35,6 +20,14 @@ and see a blinky flashed to your board!
 
 [`cargo-generate`]: https://github.com/cargo-generate/cargo-generate
 [`ravedude`]: https://github.com/Rahix/avr-hal/tree/next/ravedude
+
+## Example
+
+As an example let's implement the system for a pendulum with a propeller attached to its end. The objective would be to control the pendulum's angle relative to the "down" direction, using the motor voltage as the control variable.
+
+The system described above can be modelled the following way:
+
+$$ A(x) = a_x \cdot x + b_x \cdot x^2 $$
 
 ## License
 Licensed under either of
--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,40 @@
 use panic_halt as _;
 use arduino_hal::prelude::*;
 
+fn split_float(n: f32, power: u32) -> (u32, u32) {
+    let m = (n * power as f32) as u32;
+    (m / power, m % power)
+}
+
+macro_rules! writeln_float {
+    ($s: expr, $n: expr, $p: expr) => {{
+        let (b, c) = split_float($n, $p);
+        ufmt::uwriteln!($s, "{}.{}", b, c).void_unwrap();
+    }}
+}
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    let mut led = pins.d13.into_output();
+    // let mut led = pins.d13.into_output();
 
-    let r = [1.0; 1];
-    let mut f = [0.0; 2];
+    let r = [1_f32; 1];
+    let mut f = [0_f32; 2];
+
+    let digits = 4;
+    let pow = 10u32.pow(digits);
 
     // In this loop, [k-a] will be represented by [a]
     loop {
         f[0] = 0.99 * f[1] + 0.00995 * r[0];
 
-        // let res = buffer.format_finite(f[0]);
+        // writeln_float!(&mut serial, f[0], pow);
 
-
-        let res = (f[0] * 1e5) as u64;
-        let res1 = res / 100000;
-        let res2 = res % 100000;
-        ufmt::uwriteln!(&mut serial, "{}.{}", res1, res2)
-            .void_unwrap();
-
-        // f.rotate_left(1);  <-- This does the same as the line below (when `f.len() == 2`), but for some reason causes error in `avr-gcc` too
-        f[1] = f[0];
-
-        led.toggle();
-        arduino_hal::delay_ms(10);
+        f.rotate_left(1);
+        // f[1] = f[0];
+        arduino_hal::delay_ms(100);
     }
 }
